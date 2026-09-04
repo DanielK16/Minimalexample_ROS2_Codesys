d3743a4cccb5e6c61b2680fd5bc5bd384d4a7751
DanielK16 <koerper.daniel8@gmail.com>
2026-09-04 14:58:07 +0200

    Build, install und log Ordner aus Tracking entfernt

--- ros2_ws/build/minimalbeispiel_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/Minimalbeispiel_Codesys_ROS2/ros2_ws/install/minimalbeispiel_pkg'
--- ros2_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Minimalbeispiel_Codesys_ROS2/ros2_ws/build/turtlesim/turtlesim__py
-# Build directory: /Minimalbeispiel_Codesys_ROS2/ros2_ws/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
